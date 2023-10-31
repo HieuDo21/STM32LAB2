a4ff48f230d5c40f454622c88989534b1ee18ec3
Hieu Do <hieu.do2109@hcmut.edu.vn>
2023-11-01 01:56:52 +0700

    finish ex5

--- STM32_PROJECT/Core/Src/main.c
+++ STM32_PROJECT/Core/Src/main.c
@@ -100,6 +100,12 @@ const int MAX_LED = 4;
 	break ;
 	}
 }
+void updateClockBuffer(int hour, int minute){
+	led_buffer[0] = hour/10;
+	led_buffer[1] = hour%10;
+	led_buffer[2] = minute/10;
+	led_buffer[3] = minute%10;
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -142,8 +148,23 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int hour = 15, minute = 8, second = 50;
   while (1)
   {
+	  second ++;
+	  if(second >= 60) {
+		  second = 0;
+		  minute ++;
+	  }
+	  if(minute >= 60) {
+		  minute = 0;
+		  hour ++;
+	  }
+	  if(hour >=24){
+		  hour = 0;
+	  }
+	  updateClockBuffer(hour, minute);
+	  HAL_Delay (1000) ;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
